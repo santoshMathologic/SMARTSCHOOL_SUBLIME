2a1bf3b68e9a29d6eb013c7385d1911b30a6af13
rakesh <rak1994kumar@gmail.com>
2016-11-10 20:17:52 +0530

    header changed

--- database/db.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/smartSchool', function(error) {
-    if (error) {
-        console.log('Error Connection in Database', error);
-    } else {
-        console.log('Connection to Database Successfully');
-    }
-});
-
